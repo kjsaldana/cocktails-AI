c0f737df9a4fd849c03ef50eb2add9e5aa686fd5
Kevin J <kjoseph1910@hotmail.com>
2026-04-09 10:57:48 +0200

    fix: Add new AI model

--- src/services/AIService.ts
+++ src/services/AIService.ts
@@ -4,11 +4,12 @@ import { openrouter } from '../lib/ai'
 export default {
     async generateRecipe(prompt: string) {
         const result = streamText({
-            model: openrouter('meta-llama/llama-3.3-70b-instruct:free'),
-            // model: openrouter('google/gemma-3-4b-it:free'), <- cambiar modelo
+            model: openrouter('google/gemma-3-4b-it:free'),
+            // Otros modelos
+            // model: openrouter('qwen/qwen3-coder:free'),
             prompt,
-            // system: 'You are a bartender with 50 years of experience', <- Su comportamiento
-            // temperature: 1 <- 0 es busqueda especifica, 1 es busqueda mas creativa
+            // system: 'You are a bartender with 50 years of experience',// <- Su comportamiento
+            // temperature: 1 <- 0 es busqueda especifica,// 1 es busqueda mas creativa
         })
         return result.textStream
     }
